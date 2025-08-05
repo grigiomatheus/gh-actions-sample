24410a6a786e060fe4369bfd1287bcabfaf5a863
matheus-grigio <mgsilva14@stefanini.com>
2025-08-05 17:58:35 -0300

    fix(test-5xx): fix.

--- .releaserc.js
+++ .releaserc.js
@@ -14,17 +14,25 @@ module.exports = {
                     issuePrefixes: ["TEST-", "test-"]
                 },
                 parserOpts: {
-                    issuePrefixes: ["TEST-", "test-"],
-                    commitPattern: "^(\\w*)(?:\\((TEST-\\d+|test-\\w+)\\))?: (.*)$",
-                    headerCorrespondence: ["type", "issue", "subject"]
+                    issuePrefixes: ["TEST-", "test-"]
                 },
                 writerOpts: {
                     transform: {
+                        commit: (commit, context) => {
+                            if (commit.scope && /^TEST-|^test-/.test(commit.scope)) {
+                                const fakeRef = {
+                                    issue: `[${commit.scope}](https://atlassian.net/browse/${commit.scope})`,
+                                    raw: commit.scope
+                                };
+                                commit.references = [...(commit.references || []), fakeRef];
+                            }
+                            return commit;
+                        },
                         references: (references, context) => {
                             return references.map(ref => ({
                                 ...ref,
                                 issue: `[${ref.issue}](https://atlassian.net/browse/${ref.issue})`
-                            }))
+                            }));
                         }
                     }
                 }
