db71eee5c3083f90bdda2a2b265706d77126f8f5
matheus-grigio <mgsilva14@stefanini.com>
2025-08-05 18:33:37 -0300

    fix(TEST-123): test.

--- .release/.releaserc.js
+++ .release/.releaserc.js
@@ -1,43 +1,18 @@
 module.exports = {
-    plugins: [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                preset: "conventionalcommits"
-            }
-        ],
-        [
-            "@semantic-release/release-notes-generator",
-            {
-                preset: "conventionalcommits",
-                presetConfig: {
-                    issuePrefixes: ["TEST-", "test-"]
-                },
-                parserOpts: {
-                    issuePrefixes: ["TEST-", "test-"]
-                },
-                writerOpts: {
-                    transform: {
-                        commit: (commit, context) => {
-                            if (commit.scope && /^TEST-|^test-/.test(commit.scope)) {
-                                const fakeRef = {
-                                    issue: `[${commit.scope}](https://atlassian.net/browse/${commit.scope})`,
-                                    raw: commit.scope
-                                };
-                                commit.references = [...(commit.references || []), fakeRef];
-                            }
-                            return commit;
-                        },
-                        references: (references, context) => {
-                            return references.map(ref => ({
-                                ...ref,
-                                issue: `[${ref.issue}](https://atlassian.net/browse/${ref.issue})`
-                            }));
-                        }
-                    }
-                }
-            }
-        ],
-        "@semantic-release/github"
-    ]
-}
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      'semantic-release-jira-notes',
+      {
+        jiraHost: 'atlassian.net',
+        ticketPrefixes: ['TEST', 'test'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
