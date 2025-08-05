000a9c08eef3e0365a531199ffd31a7c7a0449dc
matheus-grigio <mgsilva14@stefanini.com>
2025-08-05 17:45:27 -0300

    fix(test-3xx): test commit.

--- .releaserc.js
+++ .releaserc.js
@@ -1,33 +1,35 @@
-export const plugins = [
-    [
-        "@semantic-release/commit-analyzer",
-        {
-            preset: "conventionalcommits"
-        }
-    ],
-    [
-        "@semantic-release/release-notes-generator",
-        {
-            preset: "conventionalcommits",
-            presetConfig: {
-                issuePrefixes: ["TEST-", "test-"]
-            },
-            parserOpts: {
-                issuePrefixes: ["TEST-", "test-"],
-                commitPattern: "^(\\w*)(?:\\((TEST-\\d+|test-\\w+)\\))?: (.*)$",
-                headerCorrespondence: ["type", "issue", "subject"]
-            },
-            writerOpts: {
-                transform: {
-                    references: (references, context) => {
-                        return references.map(ref => ({
-                            ...ref,
-                            issue: `[${ref.issue}](https://atlassian.net/browse/${ref.issue})`
-                        }))
+module.exports = {
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "conventionalcommits"
+            }
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                preset: "conventionalcommits",
+                presetConfig: {
+                    issuePrefixes: ["TEST-", "test-"]
+                },
+                parserOpts: {
+                    issuePrefixes: ["TEST-", "test-"],
+                    commitPattern: "^(\\w*)(?:\\((TEST-\\d+|test-\\w+)\\))?: (.*)$",
+                    headerCorrespondence: ["type", "issue", "subject"]
+                },
+                writerOpts: {
+                    transform: {
+                        references: (references, context) => {
+                            return references.map(ref => ({
+                                ...ref,
+                                issue: `[${ref.issue}](https://atlassian.net/browse/${ref.issue})`
+                            }))
+                        }
                     }
                 }
             }
-        }
-    ],
-    "@semantic-release/github"
-]
+        ],
+        "@semantic-release/github"
+    ]
+}
