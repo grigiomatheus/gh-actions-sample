acf5b3eb242957366d9e79cbc05cffed65fefd31
matheus-grigio <mgsilva14@stefanini.com>
2025-08-13 10:15:31 -0300

    feat!(TEST-XXX): test commit.

--- src/SampleWebApi/WeatherForecast.cs
+++ src/SampleWebApi/WeatherForecast.cs
@@ -9,7 +9,5 @@ namespace SampleWebApi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-
-        public string? Summary2 { get; set; }
     }
 }
